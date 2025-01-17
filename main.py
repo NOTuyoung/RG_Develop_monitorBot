4e5a664828c6eb4f2c0214af63b18a09a8d2bcfa
NOTuyoung <100080658+NOTuyoung@users.noreply.github.com>
2025-01-17 11:49:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def process_queue():
                         "chat_id": message_data["target_chat_id"],
                         "text": message.text or "",
                         "message_thread_id": message_data.get("message_thread_id"),
-                        "parse_mode": "HTML" # Проверяем, есть ли thread_id
+                        "parse_mode": "MarkdownV2" # Проверяем, есть ли thread_id
                     }
                 )
                 print(f"Сообщение переслано в {message_data['target_chat_id']}")
